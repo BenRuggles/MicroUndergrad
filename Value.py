cc15fd52cf05d0826de489c11ecfbd9a8903896c
BenRuggles <s4107052@student.rmit.edu.au>
2025-08-25 18:39:54 +1000

    layer norm and backprop fn's

--- Value.py
+++ Value.py
@@ -2,9 +2,9 @@ class Value:
 
     def __init__(self, data, _children=(), op='', label=''):
         self.data = data
-        # set for unique constraint
+        self.grad = 0
+        self._backward = lambda: None
         self._prev = set(_children)
-        # keep track of operation that created this node
         self.op = op
         self.label = label
 
@@ -13,11 +13,33 @@ class Value:
         return f"Value(data = {self.data})"
     
     def __add__(self, other):
-        if isinstance(other, Value):
-            return Value(self.data + other.data, (self, other), '+')
-        return Value(self.data + other)
+        out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
+
+        return out
     
     def __mul__(self, other):
-        if isinstance(other, Value):
-            return Value(self.data * other.data, (self, other), '*')
-        return Value(self.data * other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backwward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+        out._backward = _backwward
+
+        return out
+
+    def tanh(self): # hyperbolic tangent activation function
+
+        x = self.data
+        t = (2 / (1 + (-2 * x).exp())) - 1
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+        out._backward = _backward
+
+        return out
