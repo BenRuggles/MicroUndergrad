7f6bb2011ae026a7b8013572f0b358d97f0964e5
BenRuggles <s4107052@student.rmit.edu.au>
2025-08-26 08:49:49 +1000

    more methods

--- Value.py
+++ Value.py
@@ -1,3 +1,5 @@
+import math
+
 class Value: 
 
     def __init__(self, data, _children=(), op='', label=''):
@@ -13,27 +15,47 @@ class Value:
         return f"Value(data = {self.data})"
     
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other) 
         out = Value(self.data + other.data, (self, other), '+')
         
         def _backward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
+            self.grad += out.grad
+            other.grad += out.grad
         out._backward = _backward
 
         return out
     
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other) 
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backwward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
         out._backward = _backwward
 
         return out
 
-    def tanh(self): # hyperbolic tangent activation function
+    def __rmul__(self, other): # other * self
+        return self * other
+    
+    def exp(self): # exponential function
+        x = self.data
+        t = Value(math.exp(x), (self,), 'exp')
+        out = Value(t, (self,), 'tanh')
 
+        def _backward():
+            self.grad += out.data * out.grad    
+        out._backward = _backward
+
+        return out
+    
+    def __truediv__(self, other): # self / other
+        other = other if isinstance(other, Value) else Value(other)
+        return self * other**-1
+    
+
+    def tanh(self): # hyperbolic tangent activation function
         x = self.data
         t = (2 / (1 + (-2 * x).exp())) - 1
         out = Value(t, (self,), 'tanh')
@@ -43,3 +65,19 @@ class Value:
         out._backward = _backward
 
         return out
+    
+    def backward(self):
+        # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward() 
